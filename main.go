f6e1f2655ef19eda20ef503e23aed660c9ac824e
mixalispapadopoulos <mixalis.papadopoulos@gmail.com>
2025-12-07 12:21:58 +0200

    Create main.go

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	// 1. Define the handler (like a route in Flask)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello from OpenShift! You requested: %s\n", r.URL.Path)
+	})
+
+	// 2. Start the server on port 8080
+	fmt.Println("Server starting on port 8080...")
+	http.ListenAndServe(":8080", nil)
+}
